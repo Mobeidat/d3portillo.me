1cd38f8c198facbd4715ccbe9c0702335907a31d
Denny Portillo <d3portillo@gmail.com>
2020-07-11 03:36:30 -0600

    template test

--- netlify-functions/note.js
+++ netlify-functions/note.js
@@ -1,6 +1,7 @@
 const marked = require("marked")
-const NOT_FOUND_URL = `https://d3portillo.me/404`
 const fetch = require("node-fetch")
+const NOT_FOUND_URL = "https://d3portillo.me/404"
+const LAYOUT = "https://d3portillo.me/layout"
 exports.handler = (event, context, callback) => {
   const slug = event.path.replace("/notes/", "")
   const voidData = () => {
@@ -11,20 +12,27 @@ exports.handler = (event, context, callback) => {
       },
     })
   }
-
-  console.log({ slug, context, event })
   const url = `https://raw.githubusercontent.com/wiki/D3Portillo/d3portillo.me/${slug}.md`
-  fetch(url)
-    .then((r) => r.text())
-    .then((content) => {
-      const body = marked(content)
-      callback(null, {
-        statusCode: 200,
-        headers: {
-          "Content-type": "text/html",
-        },
-        body,
-      })
+  fetch(url, {
+    method: "HEAD",
+  })
+    .then((r) => {
+      if (r.ok) {
+        r.text().then((content) => {
+          const html = marked(content)
+          fetch(LAYOUT)
+            .then((r) => r.text())
+            .then((template) => {
+              callback(null, {
+                statusCode: 200,
+                headers: {
+                  "Content-type": "text/html",
+                },
+                body: template.replace("[CONTENT]", html),
+              })
+            })
+        })
+      } else voidData()
     })
     .catch(voidData)
 }
--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -3,24 +3,7 @@
     <a title="Go to notes" no-link href="/notes">
       <img src="/d.png" alt="D/Note" class="h-24 shadow" />
     </a>
-    <div v-html="content" class="mt-6"/>
+    [CONTENT]
+    <div v-html="content" class="mt-6" />
   </div>
 </template>
-<script>
-import marked from "marked"
-export default {
-  data() {
-    return {
-      content: "",
-    }
-  },
-  mounted() {
-    const url = `https://raw.githubusercontent.com/wiki/D3Portillo/D3Portillo/saber-simplemente-wow.md`
-    fetch(url)
-      .then((r) => r.text())
-      .then((content) => {
-        this.content = marked(content)
-      })
-  },
-}
-</script>
