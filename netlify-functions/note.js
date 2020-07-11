9c035520fb70d159e4fd4415a135300554b35929
Denny Portillo <d3portillo@gmail.com>
2020-07-11 04:34:18 -0600

    template test, last try before going to sleep

--- netlify-functions/note.js
+++ netlify-functions/note.js
@@ -1,7 +1,7 @@
 const marked = require("marked")
 const fetch = require("node-fetch")
 const NOT_FOUND_URL = "https://d3portillo.me/404"
-const LAYOUT = "/layout"
+const LAYOUT = "https://d3portillo.me/layout"
 exports.handler = (event, context, callback) => {
   const slug = event.path.replace("/notes/", "")
   const voidData = () => {
@@ -27,6 +27,7 @@ exports.handler = (event, context, callback) => {
                 statusCode: 200,
                 headers: {
                   "Content-type": "text/html",
+                  "Access-Control-Allow-Origin": "*",
                 },
                 body,
               })
