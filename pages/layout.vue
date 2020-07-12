a34a6fdb5dbf12290fb083675ba1c251e70fc97f
Denny Portillo <d3portillo@gmail.com>
2020-07-12 16:34:32 -0600

    note style fix

--- pages/layout.vue
+++ pages/layout.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="px-6 mx-auto max-w-5xl">
+  <div class="px-6 mx-auto max-w-5xl overflow-hidden">
     <img src="/seo_note.png" hidden />
     <a title="Go to notes" no-link href="/notes">
       <img src="/d.png" alt="D/Note" class="h-24 shadow inline" />
     </a>
-    <div class="mt-6">
+    <div class="mt-6 note-container ">
       [CONTENT]
     </div>
     <notes-footer />
