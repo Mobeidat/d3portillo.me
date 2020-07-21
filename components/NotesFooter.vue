e4ef3cbf2dbe182a1d28a1c96efd016a53e71ece
Denny Portillo <d3portillo@gmail.com>
2020-07-21 08:40:47 -0600

    iframes styling

--- components/NotesFooter.vue
+++ components/NotesFooter.vue
@@ -1,23 +1,30 @@
 <template>
-  <footer class="flex mt-20 mb-6 space-x-2 items-center">
-    <a href="/" no-link class="text-sm font-bold hover:underline">
-      D3Portillo
-    </a>
-    <div class="text-2xl font-bold">/</div>
-    <a
-      href="https://github.com/D3Portillo/d3portillo.me/wiki"
-      no-link
-      class="text-sm font-bold hover:underline"
-    >
-      Wiki
-    </a>
-    <div class="text-2xl font-bold">/</div>
-    <a
-      href="https://twitter.com/d3portillo"
-      no-link
-      class="text-sm font-bold hover:underline"
-    >
-      Twitter
-    </a>
-  </footer>
+  <div class="w-full">
+    <div class="mt-20 text-xs">
+      Thank you for reading, this are notes to myself, may contain grammar
+      errors, or are written in
+      <b>Spanish :)</b>
+    </div>
+    <footer class="flex mt-3 mb-6 space-x-2 items-center">
+      <a href="/" no-link class="text-sm font-bold hover:underline">
+        D3Portillo
+      </a>
+      <div class="text-2xl font-bold">/</div>
+      <a
+        href="https://github.com/D3Portillo/d3portillo.me/wiki"
+        no-link
+        class="text-sm font-bold hover:underline"
+      >
+        Wiki
+      </a>
+      <div class="text-2xl font-bold">/</div>
+      <a
+        href="https://twitter.com/d3portillo"
+        no-link
+        class="text-sm font-bold hover:underline"
+      >
+        Twitter
+      </a>
+    </footer>
+  </div>
 </template>
